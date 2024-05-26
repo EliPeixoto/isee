94cd5c1fee3cd7ee6bffdd39c4641683c049e0a4
EliPeixoto <eliane.peixoto@outlook.com>
2024-05-26 12:37:35 -0300

    first commit

---
+++ src/main/java/br/com/fiap/i_see/ISeeApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.i_see;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/i_see/ISeeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.i_see;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
